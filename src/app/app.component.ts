4d0b619f1399387ac6a3f02f483d90d8dc120d17
bxste <97215500+bxste@users.noreply.github.com>
2023-02-26 17:23:07 +0800

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,8 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'Angular ' + VERSION.major;
-
-  title = 'pipes-reactive';
-  presentDate = new Date();
-  //today0: Observable<Date>;
-  todayO$ = new Observable<Date>((observer) => {
-    setInterval(() => {
-      observer.next(new Date());
-    }, 1000);
-  });
-  price: number = 2000;
-  Fruits = ['Apple', 'Orange', 'Grapes', 'Mango', 'Kiwi', 'Pomegranate'];
+  title = 'pipes reactive';
+  Fruits = ['Apple', ' Orange', ' Grapes', ' Mango', ' Kiwi', ' Pear'];
   decimalNum1: number = 8.7589623;
   decimalNum2: number = 5.43;
 }
